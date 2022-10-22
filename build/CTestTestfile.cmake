daae2b797cc0e2e27963d2a7308e0988ec8a6e80
kitsou <kitsunims@icloud.com>
2022-10-22 23:31:17 +0200

    added a .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mohamed/Documents/Coding/CPP/MusicPlayer
-# Build directory: /home/mohamed/Documents/Coding/CPP/MusicPlayer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
