2644fb9ad230b1a2b3675c96609e0787193ad594
kitsou <kitsunims@icloud.com>
2022-10-22 23:25:08 +0200

    initial commit lul

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed/Documents/Coding/CPP/MusicPlayer
+# Build directory: /home/mohamed/Documents/Coding/CPP/MusicPlayer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
