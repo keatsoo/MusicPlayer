5df6181a9807630084745fef285bb4fb737e3808
kitsou <kitsunims@icloud.com>
2022-10-23 22:19:46 +0200

    added idk

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <math.h>
 #define SAMPLE_RATE 44100
 #define PLAY_DURATION 5
-#define TABLE_SIZE 880 // frequency ????
+#define TABLE_SIZE 32
+
 
 // callback, will repeatedly be called a thourought the program to compute the data
 int MusicPlayerCallback(const void* input,
@@ -19,14 +20,17 @@ PaStream* stream;
 
 // data structure that will allow the storage of the sound data
 struct MP_StreamData { float data[TABLE_SIZE]; };
-
 MP_StreamData data;
+
+double frequency = 440.0;
+double amplitude = 1.0;
 
 int main(int, char**) {
     // initialize the sine values
     for (size_t i = 0; i < TABLE_SIZE; i++)
     {
-        data.data[i] = sin( ((double) i / (double) TABLE_SIZE) * M_PIf * 2);
+        data.data[i] = (float) (amplitude * sin( frequency * 2.0 * 3.141592653589 * ((double) i) ));
+        std::cout << data.data[i] << "\n";
     }
     
 
@@ -35,7 +39,7 @@ int main(int, char**) {
     if (err != paNoError) std::cout << "PortAudio error : " << Pa_GetErrorText(err);
 
     // open a stream
-    err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, SAMPLE_RATE, 256, MusicPlayerCallback, &data);
+    err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, SAMPLE_RATE, 64, MusicPlayerCallback, &data);
 
     err = Pa_StartStream(stream);
     Pa_Sleep(PLAY_DURATION * 1000);
